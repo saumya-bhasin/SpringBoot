8c162c973157fb5a158097086a2a133704da1e68
Saumya-Bhasin <saumya.bhasin23@gmail.com>
2017-09-30 13:13:00 -0700

    Updated files.

--- course-api/src/main/java/com/Saumya/topics/TopicController.java
+++ course-api/src/main/java/com/Saumya/topics/TopicController.java
@@ -24,12 +24,24 @@ public class TopicController {
 	
 	@RequestMapping("/topics/{id}")
 	public Topic getTopic(@PathVariable String id) {
-		return topicService.getTopic("Spring");
+		return topicService.getTopic(id);
 	}
 	
 	@RequestMapping(method=RequestMethod.POST,value="/topics")
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
+		
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT,value="/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(id,topic);
+		
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE,value="/topics/{id}")
+	public void updateTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
 		
 	}
 }
